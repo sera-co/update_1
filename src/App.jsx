bc862e1fb2e536ce137e364af83af08e1be4ef40
unknown <sera.shine@kalvium.community>
2025-02-04 10:28:03 +0530

    completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,17 @@
 import UpdateItem from "./components/UpdateItem";
+import { useState } from "react";
 
-// use the following link to get the data
-// `/doors` will give you all the doors, to get a specific door use `/doors/1`.
 const API_URI = `http://${import.meta.env.VITE_API_URI}/doors`;
 
 function App() {
-  // Get the existing item from the server
-  // const [item, setItem] = useState(null);
-  // pass the item to UpdateItem as a prop
+  
+  const [itemId, setItemId] = useState('1');
+  return (
+    <div>
+      <UpdateItem itemId={itemId} />
+    </div>
+  )
 
-  return <UpdateItem />;
 }
 
 export default App;
--- src/components/UpdateItem.jsx
+++ src/components/UpdateItem.jsx
@@ -1,11 +1,50 @@
-const UpdateItem = ({ item }) => {
-    // 1. Create a state for the form
-    // 2. Create a function to handle the form submission
-    // 3. Create a function to handle the form input changes
+import { useState, useEffect } from "react";
+import axios from "axios";
 
-    // your code here
-    return null;
+const API_URI = `http://${import.meta.env.VITE_API_URI}/doors`;
+
+const UpdateItem = ({ itemId }) => {
+   
+    const [item,setItem]=useState(null)
+    const [updatedData,setUpdatedData]=useState("")
+
+    useEffect(()=>{
+        axios.get(`${API_URI}/${itemId}`)
+        .then((res)=>{
+            setItem(res.data)
+            setUpdatedData(res.data.name)
+        })
+        .catch((err)=>{
+            console.log(err)
+        })
+    },[itemId])
+
+    const handleChange=(e)=>{
+        setUpdatedData(e.target.value)
+    }
+
+    const handleUpdate=async(e)=>{
+        e.preventDefault()
+        try{
+            let response = await axios.put(`${API_URI}/${itemId}`, { name: updatedData })
+            setItem(response.data)
+            console.log('Item updated scuccessfully')
+        }
+        catch(err){
+            console.log(err.message)
+            return err
+        }
+    }
+
+
+    return (
+        <div>
+            <h1>Update Item</h1>
+            <p>name: {item?.name}</p>
+            <input type="text" value={updatedData} onChange={handleChange}/>
+            <button onClick={handleUpdate}>Update</button>
+        </div>
+    )
 };
 
 export default UpdateItem;
-
